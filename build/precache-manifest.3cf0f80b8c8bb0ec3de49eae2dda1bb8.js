3d74267c65e3aad90309e2324a308954e0477468
salim mahat <salimmahat22@gmail.com>
2020-09-14 22:49:15 +0530

    deploy on netlify

---
+++ build/precache-manifest.3cf0f80b8c8bb0ec3de49eae2dda1bb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a7f296bf635e427e6e07b347cb5a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec51aa8fa93f5c026d64",
+    "url": "/static/css/main.abd58810.chunk.css"
+  },
+  {
+    "revision": "cd4641ab908f1746ef41",
+    "url": "/static/js/2.3e591748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e591748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec51aa8fa93f5c026d64",
+    "url": "/static/js/main.d51b8c60.chunk.js"
+  },
+  {
+    "revision": "6a040985a61f21896475",
+    "url": "/static/js/runtime-main.8ccca7a7.js"
+  },
+  {
+    "revision": "37194ebb0d43ab3274bf6760a7e95262",
+    "url": "/static/media/Loading-Image-1.37194ebb.gif"
+  }
+]);
